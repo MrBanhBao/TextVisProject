71e670d0b487958ea95c497467da565306439b7e
Hao Nguyen <hao@steelix.fritz.box>
2019-07-04 08:13:32 +0200

    appended svg for results chart

--- src/D3/opposing_barchart/js/results_chart.js
+++ src/D3/opposing_barchart/js/results_chart.js
@@ -5,8 +5,17 @@ d3.json('js/data/d3_distance_result_data.json').then(function(d) {
     let yearData = d.filter(d => d.year === years[0])[0];
     console.log(yearData)
 
-    let margin = {top: 10, right: 10, bottom: 15, left: 10};
+    let margin = {top: 10, right: 0, bottom: 15, left: 0};
     let width = 600 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
+        height = 200 - margin.top - margin.bottom;
 
-};
+
+    //SVG
+    let svg = d3.select('#topvis')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height);
+
+
+
+});
